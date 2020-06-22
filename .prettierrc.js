c362e754e8ee9a1cefd7db05e1af27f0c7df5f67
femyeda <femiao@users.noreply.github.com>
2020-06-22 07:40:48 -0500

    dx: add formatting

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  printWidth: 110,
+}
