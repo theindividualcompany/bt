5159d5ae3a2f5091c8ebd616bf767c28510a223a
femyeda <femiao@users.noreply.github.com>
2020-06-22 19:22:19 -0500

    fix(desktop): not properly configuring twitter client
    - take twitter handle as well
    - update state values to match twitter values for consistency

--- packages/desktop/main/ipc.js
+++ packages/desktop/main/ipc.js
@@ -1,11 +1,27 @@
-const { ipcMain } = require('electron')
+const {ipcMain} = require('electron')
 const {set} = require('./config')
 
 module.exports = (app, window) => {
   ipcMain.on('set-login-credentials', async (event, payload) => {
-    await set('auth', payload)
+    const {access_token_key, access_token_secret, consumer_key, consumer_secret, handle} = payload
+
+    await set('auth', {
+      access_token_key,
+      access_token_secret,
+      consumer_key,
+      consumer_secret,
+    })
+
+    await set('handle', handle)
+
+    window.bt = bt.configure({
+      access_token_key,
+      access_token_secret,
+      consumer_key,
+      consumer_secret,
+    })
   })
-  
+
   ipcMain.on('logout', async (event, payload) => {
     await set('auth', {})
   })
