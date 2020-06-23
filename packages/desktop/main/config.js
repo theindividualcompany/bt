6a6c0817e8aac222b3346bcf23972878ec740e1d
femyeda <femiao@users.noreply.github.com>
2020-06-22 19:24:50 -0500

    feat(desktop): extend config to allowing requesting data

--- packages/desktop/main/config.js
+++ packages/desktop/main/config.js
@@ -2,18 +2,23 @@ const {app} = require('electron')
 const path = require('path')
 const Store = require('electron-store')
 
-const { platform } = process
+const {platform} = process
 const isWindows = platform === 'win32'
 const isMacOS = platform === 'darwin'
 
-const getStore = (opts) => {
-  opts = Object.assign({ defaults: { } }, opts);
+const getStore = opts => {
+  opts = Object.assign({defaults: {}}, opts)
   return new Store(opts)
 }
 
 exports.store = getStore()
 
-exports.set = async (path, data) => {
+exports.set = async (key, data) => {
   const store = getStore()
-  store.set(path, data)
+  store.set(key, data)
+}
+
+exports.get = async key => {
+  const store = getStore()
+  return store.get(key)
 }
