23b443d5b6863f548676b8fe83b7bfcf58a2da84
femyeda <femiao@users.noreply.github.com>
2020-06-24 09:55:41 -0500

    chore(desktop): index coming soon

--- packages/desktop/renderer/pages/index.js
+++ packages/desktop/renderer/pages/index.js
@@ -1,15 +1,25 @@
-import Link from 'next/link'
-import { useEffect } from 'react'
+import {useState, useEffect} from 'react'
+import Screen from '../components/Screen'
+import Navigation from '../components/Navigation'
+
 import ipc from '../utils/ipc'
 
 export default () => {
+  const [profile, setProfile] = useState({})
+
+  useEffect(() => {
+    ;(async () => {
+      let profile = await ipc.getProfile()
+      setProfile(profile)
+    })()
+  }, [])
 
   return (
-    <>
-      <span>This is Next.js speaking</span>
-      <Link href='/another'>
-        <a>another</a>
-      </Link>
-    </>
+    <Screen>
+      <Navigation active='/index' profile={profile} />
+      <main className='h-64 text-center bg-gray-100 border border-gray-400 rounded-md p-2 flex flex-col content-center justify-center'>
+        <p>CIRCLELY COMING SOON</p>
+      </main>
+    </Screen>
   )
 }
