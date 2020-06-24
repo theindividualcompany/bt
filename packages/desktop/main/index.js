54d56a42d9e0d783a2c046ec810d9a8b5bfe43f2
femyeda <femiao@users.noreply.github.com>
2020-06-24 09:21:11 -0500

    feat(desktop): set start page followers

--- packages/desktop/main/index.js
+++ packages/desktop/main/index.js
@@ -64,8 +64,8 @@ app.on('ready', async () => {
   }
 
   const mainWindow = createWindow()
-
   prepareIpc(app, mainWindow)
+
   app.on('activate', function() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
