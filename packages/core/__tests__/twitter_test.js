ac8d3125b19ef2a2567d816fa521c69a36523154
femyeda <femiao@users.noreply.github.com>
2020-06-25 20:02:39 -0500

    feat(core): require message passed to newCampaign

--- packages/core/__tests__/twitter_test.js
+++ packages/core/__tests__/twitter_test.js
@@ -9,13 +9,11 @@ let auth = {
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 }
 
-
 ;(async function() {
   log.setLevel('trace')
   let user_name = 'femyeda'
 
-
-  const bt = core.configure(auth)
+  const bt = core.configure(auth, './configs')
   const scanner = bt.scanner
   await bt.scanner.setConfUsername(user_name)
 
@@ -28,27 +26,34 @@ let auth = {
   // await scanner.setConfUsername(user_name)
   // scanner.scan()
 
-  let camp_params =
-      {
-        num_batch:1,
-        // whitelist:["femyeda","DevScrape"],
-        // blacklist:["femyeda"],
-        dry_run:true
+  let camp_params = {
+    num_batch: 10,
+    // whitelist:["femyeda","DevScrape"],
+    // blacklist:["femyeda"],
+    message: 'this message',
+    dry_run: true,
+  }
+  //if dry_run false, then all dry_runs will be filtered out when calculating a batch, but you can still delete to get rid of unnecessary dry_run data
+  if (!camp_params.dry_run) {
+    scanner.clearCampaignDryRuns()
+  }
+
+  let campaign = scanner
+    .getNewCampaign(camp_params)
+    .then(v => {
+      if (v.dry_run) {
+        console.log('dray run')
+        v.campaign_users.forEach(e => {
+          console.log(`Sending DM "${v.message}" to ${e.screen_name}`)
+          v.sent_to.push(e.id_str)
+          // v.message = "DRY RUN MESSAGE"
+        })
       }
-      //if dry_run false, then all dry_runs will be filtered out when calculating a batch, but you can still delete to get rid of unnecessary dry_run data
-      if(!camp_params.dry_run){scanner.clearCampaignDryRuns();}
 
-  let campaign = scanner.getNewCampaign(camp_params).then((v) => {
-
-    v.campaign_users.forEach(e => {
-      console.log("Sending DM to " + e.screen_name)
-      v.sent_to.push(e.id_str);
-      v.message = "DRY RUN MESSAGE"
+      scanner.storeCompletedCampaign(v)
+      // console.log(v)
     })
-
-    scanner.storeCompletedCampaign(v)
-    // console.log(v)
-  }).catch(err => console.log(err))
+    .catch(err => console.log(err))
   // let users = await scanner.gatewayAPI.getUsersLookup(["femyeda"])
   // console.log(users)
   // let userTweetsWithRetweets = await TwitterProc.getUserTimelineTweetsWithRetweeterIDs(user_name, 20);
